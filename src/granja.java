b19175b7331a6014ab8599426771d1dafc27541d
hadolfov <hadolfov@gmail.com>
2019-12-13 17:34:39 -0600

    seg

--- src/granja.java
+++ src/granja.java
@@ -4,12 +4,13 @@ import java.awt.event.KeyEvent;
 
 public class granja {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Granja G = new Granja();
         do {
             G.nuevoAnimal();
             G.hacerBulla();
             System.out.println("---------");
+            Thread.sleep(500);
         } while (1 == 1);
     }
 }
